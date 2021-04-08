01a9ab9027af64dfeeb5257e374ebb321c9cfc00
Wilian Da Silva Janez <A96907@somoseducacao.com.br>
2021-04-08 00:29:55 -0300

    ranking

--- src/app/views/draw-sort/draw-sort.component.ts
+++ src/app/views/draw-sort/draw-sort.component.ts
@@ -7,25 +7,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DrawSortComponent implements OnInit {
   datas: [string];
+  newArray: [string];
   data: any;
   drawn: any;
 
   constructor() { 
     this.datas = [''];
+    this.newArray = [''];
   }
 
   ngOnInit(): void {
-    this.datas = [''];
+
   }
 
   addValue(): void {
-    this.datas.push(this.data);
-    this.data = '';
+    if (this.data.length > 0) {
+      this.datas.push(this.data);
+      this.data = '';
+    }
   }
 
   draw(): void {
     let index = Math.floor(Math.random() * this.datas.length);
     let value = this.datas[index];
     this.drawn = value;
+  }
+
+  rank(): void {
+    this.datas = this.datas.sort(function (a, b){ 
+      return Math.floor(Math.random() * 10);
+    });    
+    this.datas = this.ranking(this.datas);
+
+    console.log('foi!');
+  }
+
+  ranking(values = ['']) {
+    this.newArray = [''];
+    let number = Math.floor(Math.random() * values.length);
+    let count = 1;
+    this.newArray.push(values[number]);
+
+    while (count < values.length - 1) {
+        const newNumber = Math.floor(Math.random() * values.length);
+        if (!this.newArray.includes(values[newNumber])) {
+            count++;
+            number = newNumber;
+            this.newArray.push(values[number]);
+        }
+    }
+
+    return this.newArray;
   }
 }
